c8bcd70fe00062b87b6a2233bf2aca757b4862ed
Jhonjhorie <74289488+Jhonjhorie@users.noreply.github.com>
2023-12-04 03:05:36 +0800

    added POS

---
+++ Repository/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using SBIT3J_SuperSystem_Final.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SBIT3J_SuperSystem_Final.Repository
+{
+    public class ProductRepository
+    {
+        private DatabaseConnectionEntities objSBIT3JEntities;
+
+        public ProductRepository()
+        {
+            objSBIT3JEntities = new DatabaseConnectionEntities();
+        }
+        public IEnumerable<SelectListItem> GetAllProduct()
+        {
+            IEnumerable<SelectListItem> objSelectListItems = new List<SelectListItem>();
+            objSelectListItems = (from obj in objSBIT3JEntities.Product_Info
+                                  select new SelectListItem()
+                                  {
+                                      Text = obj.Product_Code,
+                                      Value = obj.Product_ID.ToString(),
+                                      Selected = true
+                                  }).ToList();
+            return objSelectListItems;
+        }
+    }
+}
---
+++ Repository/TransactionRepository.cs
@@ -0,0 +1,43 @@
+﻿using SBIT3J_SuperSystem_Final.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SBIT3J_SuperSystem_Final.Repository
+{
+    public class TransactionRepository
+    {
+
+        private DatabaseConnectionEntities objSBIT3JEntities;
+
+        public TransactionRepository()
+        {
+            objSBIT3JEntities = new DatabaseConnectionEntities();
+        }
+        public bool AddOrder(Sales_Transaction objSalesTransaction)
+        {
+            Sales_Transaction objSaleTransaction = objSalesTransaction;
+
+            objSaleTransaction.Account_ID = objSalesTransaction.Account_ID;
+            objSaleTransaction.Total_Amount = objSalesTransaction.Total_Amount;
+            objSaleTransaction.Date = DateTime.Now;
+            objSBIT3JEntities.Sales_Transaction.Add(objSaleTransaction);
+            objSBIT3JEntities.SaveChanges();
+            int TransactionID = objSaleTransaction.Transaction_ID;
+
+            foreach (var item in objSaleTransaction.ListofOrderDetailViewModel)
+            {
+                Sales_Transaction_Details objSalesTransactionDetail = new Sales_Transaction_Details();
+                objSalesTransactionDetail.Transaction_ID = TransactionID;
+                objSalesTransactionDetail.Product_ID = item.Product_ID;
+                objSalesTransactionDetail.Total_Quantity = (-1) * item.Total_Quantity;
+                objSalesTransactionDetail.Discount_ID = item.Discount_ID;
+                objSBIT3JEntities.Sales_Transaction_Details.Add(objSalesTransactionDetail);
+                objSBIT3JEntities.SaveChanges();
+            }
+
+            return true;
+        }
+    }
+}
