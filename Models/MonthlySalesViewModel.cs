70973260b4850090d46a704bb9bba04023fee3cd
SheeeChaos <146656845+SheeeChaos@users.noreply.github.com>
2023-12-11 13:06:50 +0800

    DATA GRAPH

---
+++ Models/MonthlySalesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SBIT3J_SuperSystem_Final.Models
+{
+    public class MonthlySalesViewModel
+    {
+        public string Month { get; set; }
+        public decimal? TotalSales { get; set; }
+    }
+
+    public class SalesGraphViewModel
+    {
+        public List<MonthlySalesViewModel> MonthlySales { get; set; }
+    }
+}
